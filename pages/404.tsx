071fda9d6915d583ff0d39440102d5c607b0b587
nareshban1 <np03a170060@heraldcollege.edu.np>
2022-12-13 22:35:37 +0545

    feat: basic 404 ppage changes.

--- pages/404.tsx
+++ pages/404.tsx
@@ -4,21 +4,25 @@ import React from "react";
 
 const CustomPageNotFound = () => {
   return (
-    <section className="mt-5 text-center">
+    <section className="py-5 text-center bg-primary">
       <Image
         src="/connection.svg"
         alt="Gripas Marketing"
         width={400}
         height={250}
+        className="my-5"
       />
-      <div className="text-center mt-5">
-        <p className="fs-1 fw-bold">404 Not Found</p>
-        <p className="m-4 ">
+      <div className="text-center  text-white my-5">
+        <h1 className="fs-1 fw-bold">404 Not Found</h1>
+        <p className="m-4 lead text-white fw-medium hero-sub-text">
           Sorry, we can&apos;t find that page. You&apos;ll find lots to explore
           on the home page.{" "}
         </p>
-        <div className="flex justify-content-center w-100 text-center">
-          <Link href={"/"} className="btn btn-primary rounded-pill px-4">
+        <div className="text-center">
+          <Link
+            href={"/"}
+            className="btn btn-outline-white my-5 rounded-0 px-4 py-3 nav-link-text "
+          >
             Back to Homepage
           </Link>
         </div>
